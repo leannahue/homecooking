2c0f04f976075c5b048c44e133e83a4b2f950e6d
Leanna Hue <leannahue@wirelessprvnat-172-16-209-172.near.illinois.edu>
2018-02-25 16:05:33 -0600

    almost finished implementing api

--- generateRecipes.js
+++ generateRecipes.js
@@ -1,96 +1,110 @@
 ingredientstr = localStorage["ingredientstr"]
 var recipe_names = [];
 var recipe_images = [];
-
+var recipe_array = [];
 // function displayIngredients() {
 // 	console.log("hello");
 // 	console.log(ingredientstr);
+//   console.log(typeof(ingredientstr))
 // 	document.getElementById("ingredientList").innerHTML = ingredientstr;
 // }
 //
 // displayIngredients();
 
-var recipe_array = [];
-// var F2Fkey = "a424e4c0845023455bc2060bf36593a7";
-var F2Fkey = "1bb35aab149d54449a70218d016a6d28";
+function generateList(str, names, images) {
 
-// $.ajax({
-//   url: "http://food2fork.com/api/search",
-//   type: "GET",
-//   data: {
-//     key: F2Fkey,
-//     q: ingredientstr,
-//     sort: "sort=r",
-//     headers: {
-//       'Access-Control-Allow-Origin': '*',
-//       'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-//       'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
-//     },
-//     page: 1
-//   },
-//   contentType: 'application/json',
-//   success: function(response) {
-//     response = JSON.parse(response);
-//     // console.log("About to get each recipe...");
-//     // response["recipes"].forEach((dish) => {
-//     for(i = 0; i < response["recipes"].length; i++) {
-//       $.ajax({
-//         url: "http://food2fork.com/api/get",
-//         type: 'GET',
-//         data: {
-//           key: F2Fkey,
-//           rId: response["recipes"][i].recipe_id,
-//         },
-//         success: function(recipe_item) {
-//           // console.log('Success in finding one recipe!');
-//           // console.log(typeof recipe); string;
-//           recipe_item = JSON.parse(recipe_item);
-//           recipe_array.push(recipe_item);
-//           // console.log(recipe_item);
-//           recipe_names.push(recipe_item.recipe.title);
-//           recipe_images.push(recipe_item.recipe.image_url);
-//           // console.log(recipe_array);
-//           // recipe_array = recipe_array.concat(recipe);
-//         },
-//         error: function(error) {
-//           console.log(error);
-//         }
-//       })
-//     }
-//   },
-//   error: function(xhr) {
-//     console.log("ERROR!!!", xhr);
-//   }
-// }).done(() => {
-//   console.log(recipe_array);
-//   // console.log(recipe_array.length); // undefined
-//   // console.log(typeof recipe_array); // object
-//   // for (var i = 0; i < recipe_names.length; i++) {
-//   //   // recipe_names[i] = recipe_names[i].replace(/&amp;/g,"&");
-//   // }
-//   console.log(recipe_names);
-//   integrateRecipes();
-//   // $('.recipes').html(finish_page);
-// })
+  // var recipe_array = [];
+  var array = [];
+  var F2Fkey = "a424e4c0845023455bc2060bf36593a7";
+  // var F2Fkey = "1bb35aab149d54449a70218d016a6d28";
 
-recipe_names = ["Beef-Pesto Panini"]
-recipe_images = ["https://images-gmi-pmc.edge-generalmills.com/f1a87202-9df0-40eb-a627-71d10c1c5bd8.jpg"]
-integrateRecipes();
+$.ajax({
+  url: "http://food2fork.com/api/search",
+  type: "GET",
+  data: {
+    key: F2Fkey,
+    q: ingredientstr,
+    sort: "sort=r",
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
+    },
+    page: 1
+  },
+  contentType: 'application/json',
+  success: function(response) {
+    response = JSON.parse(response);
+    // console.log("About to get each recipe...");
+    // response["recipes"].forEach((dish) => {
+    for(i = 0; i < response["recipes"].length; i++) {
+      $.ajax({
+        url: "http://food2fork.com/api/get",
+        type: 'GET',
+        data: {
+          key: F2Fkey,
+          rId: response["recipes"][i].recipe_id,
+        },
+        success: function(recipe_item) {
+          // console.log('Success in finding one recipe!');
+          // console.log(typeof recipe); string;
+          recipe_item = JSON.parse(recipe_item);
+          array.push(recipe_item.recipe);
+          // console.log(recipe_item);
+          names.push(recipe_item.recipe.title);
+          console.log(recipe_item.recipe.title);
+          console.log(names);
+          images.push(recipe_item.recipe.image_url);
+          // console.log(recipe_array);
+          // recipe_array = recipe_array.concat(recipe);
+        },
+        error: function(error) {
+          console.log(error);
+        }
+      })
+    }
+    integrateRecipes();
+  },
+  error: function(xhr) {
+    console.log("ERROR!!!", xhr);
+  }
+}).done(() => {
 
-function integrateRecipes() {
-  // console.log("At least we entered the function...");
-  // console.log(recipe_names);
-  // console.log(recipe_names.length);
+  // $('.recipes').html(finish_page);
+})
+  return [array, names, images];
+}
+
+function integrateRecipes(names, images) {
+  console.log(names);
   text = '<div class="row">';
-  for (var i = 0; i < recipe_names.length; i++) {
-    console.log("Creating div for " + recipe_names[i] + " at index " + i);
+  for(var i = 0; i < 30; i++){
+    console.log('recipe key:', i);
+    console.log("Creating div for " + names[i] + " at index " + i);
     if (i % 3 == 0 && i != 0) { // rows have 3 things each
       text = text + '</div><div class="row">'; // start a new row
     }
-    text = text + '<div class="col-sm-4"><img onclick="choseRecipe(' + i + ')" class="recommended-recipes-image" src=' + recipe_images[i] + '><span id="r1" class="recommended-recipes-name">' + recipe_names[i] + '</span><span class="recommended-recipes-cooktime"> ' + (Math.floor(Math.random() * 30) + 15) + ' minutes </span></div>';
+    text = text + '<div class="col-sm-4"><img onclick="choseRecipe(' + i + ')" class="recommended-recipes-image" src=' + recipe_images[i] + '><span id="r1" class="recommended-recipes-name">' + recipe_names[i] + '</span><span class="recommended-recipes-cooktime"> ' + (Math.floor(Math.random() * 30) + 15) + ' minutes </span><span class="ready-indicator"> You have all the ingredients to make this! </span></div>';
+
   }
   document.getElementById("recipes").innerHTML = text;
 }
+
+[recipe_array, recipe_names, recipe_images] = generateList(ingredientstr, recipe_names, recipe_images);
+setTimeout(function(){
+  console.log("////////////////////")
+  console.log(recipe_array);
+  console.log(recipe_names);
+  console.log(recipe_images);
+  integrateRecipes(recipe_names, recipe_images);
+}, 6000);
+
+integrateRecipes(recipe_names, recipe_images);
+// recipe_names = ["Beef-Pesto Panini"]
+// recipe_images = ["https://images-gmi-pmc.edge-generalmills.com/f1a87202-9df0-40eb-a627-71d10c1c5bd8.jpg"]
+// integrateRecipes();
+
+
 
 function choseRecipe(index) {
   console.log(recipe_names);
@@ -106,4 +120,56 @@ function choseRecipe(index) {
   window.location.href = "recipe.html" + queryString;
   console.log(window.location);
   // localStorage["ingredientstr"] = chosen_recipe;
+}
+
+function loadingBar() {
+  var elem = document.getElementById("progressBar");
+  var width = 1;
+  var id = setInterval(frame, 60);
+  var done = 0;
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+      done = 1;
+    } else {
+      width++;
+      elem.style.width = width + '%';
+    }
+  }
+  if (done == 1) {
+    switchContainer();
+  }
+}
+
+loadingBar();
+
+function switchContainer() {
+  var firstContainer = document.getElementById("over-container");
+  var secondContainer = document.getElementById("container");
+  var changeOne = setInterval(first, 1000);
+
+  function first() {
+  	firstContainer.style.opacity = 0;
+  	firstContainer.style.animationName = "fadeInOut";
+  	firstContainer.style.animationDelay = "8.5s";
+  	firstContainer.style.animationDuration = "9s";
+  	var changeTwo = setInterval(second, 500);
+  }
+
+  function second() {
+  	secondContainer.style.opacity = 1;
+  	secondContainer.style.animationName = "fadeIn";
+  	secondContainer.style.animationDelay = "8.5s";
+  	secondContainer.style.animationDuration = "9s";
+  	$('#over-container').css({display: 'none'})
+    // var fadeSecond = setInterval(fadeTwo, 2000);
+  }
+
+  // function fadeTwo() {
+  //   secondContainer.style.opacity = 0;
+  // 	secondContainer.style.animationName = "fadeInOut";
+  // 	secondContainer.style.animationDelay = "8.5s";
+  // 	secondContainer.style.animationDuration = "5s";
+  //   var nextPage = setInterval(Page2, 500);
+  // }
 }
