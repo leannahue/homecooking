86de0547f89c9fc78be73ca894f3f28c396228f7
mikelim724 <36488622+mikelim724@users.noreply.github.com>
2018-02-28 11:05:45 -0600

    Update autopopulation

--- meal-plan.js
+++ meal-plan.js
@@ -2,6 +2,13 @@ var notshowing = true;
 
 function auto() {
 	if (notshowing) {
+	document.getElementById("meal-mon-break").classList.toggle("show");
+	document.getElementById("meal-tues-break").classList.toggle("show");
+	document.getElementById("meal-wed-break").classList.toggle("show");
+	document.getElementById("meal-thurs-break").classList.toggle("show");
+	document.getElementById("meal-fri-break").classList.toggle("show");
+	document.getElementById("meal-sat-break").classList.toggle("show");
+	document.getElementById("meal-sun-break").classList.toggle("show");
 	document.getElementById("meal-mon").classList.toggle("show");
 	document.getElementById("meal-tues").classList.toggle("show");
 	document.getElementById("meal-wed").classList.toggle("show");
@@ -9,6 +16,13 @@ function auto() {
 	document.getElementById("meal-fri").classList.toggle("show");
 	document.getElementById("meal-sat").classList.toggle("show");
 	document.getElementById("meal-sun").classList.toggle("show");
+	document.getElementById("meal-mon-din").classList.toggle("show");
+	document.getElementById("meal-tues-din").classList.toggle("show");
+	document.getElementById("meal-wed-din").classList.toggle("show");
+	document.getElementById("meal-thurs-din").classList.toggle("show");
+	document.getElementById("meal-fri-din").classList.toggle("show");
+	document.getElementById("meal-sat-din").classList.toggle("show");
+	document.getElementById("meal-sun-din").classList.toggle("show");
 	notshowing = false;
 	}
 }
